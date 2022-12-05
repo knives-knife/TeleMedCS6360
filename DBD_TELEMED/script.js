71b329f4cc11d36cea3ba1d9d3202357d53d2380
knives-knife <93448980+knives-knife@users.noreply.github.com>
2022-12-04 21:05:12 -0600

    Initial Commit

---
+++ DBD_TELEMED/script.js
@@ -0,0 +1,53 @@
+async function getDataById(id){
+    let data = await fetch('http://localhost:3300/admins/'+id);
+    data = await data.json();
+    console.log('res', data.data[0].first_name);
+    document.getElementById('username').innerText = "Hello, "+ data.data[0].first_name;
+}
+
+async function getFormData(e){
+    e.preventDefault();
+
+    let username = document.getElementById('username_ip').value;
+    let pass = document.getElementById('password_ip').value;
+
+    let basePath = window.location.pathname.substring(0, window.location.pathname.lastIndexOf('/')+1);
+    console.log(basePath);
+
+    let options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username: username,
+            pass: pass
+        })
+    }
+    let resp = await fetch('http://localhost:3300/admins', options)
+    resp = await resp.json();
+
+    console.log(resp);
+    console.log(resp.data);
+
+    // Authenticated
+    if (resp.data.admin_id > 0)
+    {
+        window.location.href = basePath + 'admin/index.html'
+    }
+    // Not Authenticated
+    else if (resp.data.admin_id == -1)
+    {
+        alert("Incorrect username and/or password");
+    }
+    // Error
+    else
+    {
+        alert("ERROR")
+    }
+
+    // alert('Message is :' + resp);
+    //window.location.href = 'C:/Users/sudob/Desktop/DBD_TELEMED/register.html'
+}
+
+getDataById(2);
