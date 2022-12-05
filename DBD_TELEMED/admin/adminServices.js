5581936f5b7cc64c40f8c388311a86ec349a3c07
knives-knife <93448980+knives-knife@users.noreply.github.com>
2022-12-05 01:19:21 -0600

    Combine Add and Save Patient in AdminServices

--- DBD_TELEMED/admin/adminServices.js
+++ DBD_TELEMED/admin/adminServices.js
@@ -37,60 +37,15 @@ async function getPatientById(id) {
     return resp;
 }
 
-async function addPatient(e) {
-    e.preventDefault();
-
-    var patient = {
-        PatientId: 0,
-        FName: "",
-        MName: "",
-        LName: "",
-        Gender: "",
-        DateOfBirth: Date.now,
-        PhoneNumber: "",
-        Address: "",
-        Username: "",
-        Password: "",
-    }
-
-    let vals = [...e.target.elements];
-    vals.forEach(x => {
-        if (patient[x.name] !== undefined || patient[x.name] != null) {
-            patient[x.name] = x.value;
-        }
-    })
-
-    patient.Gender = patient.Gender.substring(0, 1);
-
-    let basePath = getBasePath(window.location.pathname, 2);
-
-    let options = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(patient)
-    }
-    let resp = await fetch('http://localhost:3300/admin/SavePatient', options)
-    resp = await resp.json();
-
-    // Authenticated
-    if (resp.Code === "SUCCESS") {
-        alert(resp.Message);
-        window.location.href = basePath + 'admin/patient.html'
-    }
-    // Error
-    else {
-        alert("ERROR: " + resp.Message);
-    }
-}
-
 async function savePatient(e) {
     let resp = undefined;
 
     e.preventDefault();
 
-    let id = parent.document.URL.substring(parent.document.URL.indexOf('=') + 1, parent.document.URL.length);
+    let id = "0";
+    if (parent.document.URL.indexOf('=') > -1) {
+        id = parent.document.URL.substring(parent.document.URL.indexOf('=') + 1, parent.document.URL.length);
+    }
 
     var patient = {
         PatientId: id,
@@ -136,8 +91,7 @@ async function savePatient(e) {
         window.location.href = basePath + 'admin/patient.html'
     }
     // Invalid Input
-    else if (resp.Code == "INVALID")
-    {
+    else if (resp.Code == "INVALID") {
         alert(resp.Message);
     }
     // Error
